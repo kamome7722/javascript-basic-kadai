25164b5d5b9add9d91eb0c062e1661f70c1c3dc0
kamome7722 <143251663+kamome7722@users.noreply.github.com>
2023-11-03 15:21:51 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,4 +1,4 @@
-const num = 5;
+const num = 15;
 
 console.log(num); 
 
@@ -10,8 +10,8 @@ else if (num % 5 === 0){
     console.log("5の倍数です");
 }
 
-if(num % 3 === 0 && num % 5 === 0){
-    console.log("3と5の倍数です")
+else if(num % 3 === 0 && num % 5 === 0){
+    console.log("3と5の倍数です");
 }
 
 else {
