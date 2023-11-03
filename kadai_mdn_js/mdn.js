39206e04c1bc848911c66752b281f99c4d6aa63a
kamome7722 <143251663+kamome7722@users.noreply.github.com>
2023-11-03 18:14:14 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+var dateName = new date([2022,8,22]);
+
+var year=dateName.getFullYear();
+var month=dateName.getMonth();
+var day=dateName.getDay();
+
+console.log(year+'年');
+console.log(month+'月');
+console.log(day+'日');
